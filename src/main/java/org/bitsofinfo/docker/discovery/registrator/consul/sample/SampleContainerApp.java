15bc7d255b9217bd125473342053a927cd280a03
bitsofinfo <bitsofinfo.g@gmail.com>
2016-03-23 16:40:39 -0600

    tabs

--- src/main/java/org/bitsofinfo/docker/discovery/registrator/consul/sample/SampleContainerApp.java
+++ src/main/java/org/bitsofinfo/docker/discovery/registrator/consul/sample/SampleContainerApp.java
@@ -22,92 +22,92 @@ import org.bitsofinfo.docker.discovery.registrator.consul.ServiceNameStrategy;
  */
 public class SampleContainerApp implements Runnable {
 
-	/**
-	 * Start the app with a background thread that reports state
-	 * 
-	 * @param args
-	 * @throws Exception
-	 */
-	public static void main(String args[]) throws Exception {
-		SampleContainerApp app = new SampleContainerApp();
+    /**
+     * Start the app with a background thread that reports state
+     * 
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String args[]) throws Exception {
+        SampleContainerApp app = new SampleContainerApp();
 
-		Thread t = new Thread(app);
-		t.start();
+        Thread t = new Thread(app);
+        t.start();
 
-		Thread.sleep(1000*60*30); // exit in 30 m
-	}
-
-
-	private String myUniqueTag;
-	private ConsulDiscovery consulDiscovery = null;
-
-	/**
-	 * Constructor
-	 */
-	@SuppressWarnings("unchecked")
-	public SampleContainerApp() {
-
-		try {
-			/*
-			 * All the System variables below are obtained via
-			 * -D System property flags set on the JVM when the 
-			 * app is launched. See example in README.
-			 * 
-			 * Note this is not the only way you can do this
-			 * as there are numerous ways to get this information 
-			 * into a container. This is just an example
-			 */
-			myUniqueTag= System.getProperty("MY_UNIQUE_TAG");
-
-			// construct our ConsulDiscovery using builder methods
-			consulDiscovery = new ConsulDiscovery()
-					.setConsulIp(System.getProperty("CONSUL_IP"))
-					.setConsulPort(Integer.valueOf(System.getProperty("CONSUL_PORT")))
-					.setServiceName(System.getProperty("MY_SERVICE_NAME")) 
-					.setMyNodeUniqueTagId(myUniqueTag)
-					.addPortToDiscover(8080)
-					.addPortToDiscover(8443)
-					.setServiceNameStrategyClass(
-							(Class<? extends ServiceNameStrategy>)
-							Class.forName(System.getProperty("SERVICE_NAME_STRATEGY")));
-
-		} catch(Exception e) {
-			throw new RuntimeException("Error constructing SampleContainerApp " + e.getMessage(),e);
-		}
-	}
+        Thread.sleep(1000*60*30); // exit in 30 m
+    }
 
 
-	@Override 
-	public void run() {
-		try {
-			while(true) {
-				Thread.sleep(10000); 
+    private String myUniqueTag;
+    private ConsulDiscovery consulDiscovery = null;
 
-				// Lets discover ServiceInfo about myself 
-				Collection<ServiceInfo> myServices = consulDiscovery.discoverMe();
-				
-				// and my peers....
-				Collection<ServiceInfo> myPeerServices = consulDiscovery.discoverPeers();
+    /**
+     * Constructor
+     */
+    @SuppressWarnings("unchecked")
+    public SampleContainerApp() {
 
-				StringBuffer sb = new StringBuffer("########## " + myUniqueTag + " REPORTING: ##########\n");
+        try {
+            /*
+             * All the System variables below are obtained via
+             * -D System property flags set on the JVM when the 
+             * app is launched. See example in README.
+             * 
+             * Note this is not the only way you can do this
+             * as there are numerous ways to get this information 
+             * into a container. This is just an example
+             */
+            myUniqueTag= System.getProperty("MY_UNIQUE_TAG");
 
-				sb.append("MY SERVICES: \n");
-				for (ServiceInfo info : myServices) {
-					sb.append(info.getExposedAddress() + ":" + info.getExposedPort() + " -> container:"+ info.getMappedPort()+" tags: "+Arrays.asList(info.getTags().toArray())+"\n");
-				}
+            // construct our ConsulDiscovery using builder methods
+            consulDiscovery = new ConsulDiscovery()
+                    .setConsulIp(System.getProperty("CONSUL_IP"))
+                    .setConsulPort(Integer.valueOf(System.getProperty("CONSUL_PORT")))
+                    .setServiceName(System.getProperty("MY_SERVICE_NAME")) 
+                    .setMyNodeUniqueTagId(myUniqueTag)
+                    .addPortToDiscover(8080)
+                    .addPortToDiscover(8443)
+                    .setServiceNameStrategyClass(
+                            (Class<? extends ServiceNameStrategy>)
+                            Class.forName(System.getProperty("SERVICE_NAME_STRATEGY")));
 
-				sb.append("\n\nMY PEER SERVICES: \n");
-				for (ServiceInfo info : myPeerServices) {
-					sb.append(info.getExposedAddress() + ":" + info.getExposedPort() + " -> container:"+ info.getMappedPort()+" tags: "+Arrays.asList(info.getTags().toArray())+"\n");
-				}
+        } catch(Exception e) {
+            throw new RuntimeException("Error constructing SampleContainerApp " + e.getMessage(),e);
+        }
+    }
 
-				sb.append("\n########## END " + myUniqueTag + " ############\n\n");
 
-				System.out.println(sb.toString());
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @Override 
+    public void run() {
+        try {
+            while(true) {
+                Thread.sleep(10000); 
+
+                // Lets discover ServiceInfo about myself 
+                Collection<ServiceInfo> myServices = consulDiscovery.discoverMe();
+                
+                // and my peers....
+                Collection<ServiceInfo> myPeerServices = consulDiscovery.discoverPeers();
+
+                StringBuffer sb = new StringBuffer("########## " + myUniqueTag + " REPORTING: ##########\n");
+
+                sb.append("MY SERVICES: \n");
+                for (ServiceInfo info : myServices) {
+                    sb.append(info.getExposedAddress() + ":" + info.getExposedPort() + " -> container:"+ info.getMappedPort()+" tags: "+Arrays.asList(info.getTags().toArray())+"\n");
+                }
+
+                sb.append("\n\nMY PEER SERVICES: \n");
+                for (ServiceInfo info : myPeerServices) {
+                    sb.append(info.getExposedAddress() + ":" + info.getExposedPort() + " -> container:"+ info.getMappedPort()+" tags: "+Arrays.asList(info.getTags().toArray())+"\n");
+                }
+
+                sb.append("\n########## END " + myUniqueTag + " ############\n\n");
+
+                System.out.println(sb.toString());
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
