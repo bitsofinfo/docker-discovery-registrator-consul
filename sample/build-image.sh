ea846cf7fff21c4c545a5b2ca55dbd67a501d9ce
bitsofinfo <bitsofinfo.g@gmail.com>
2016-03-23 16:25:17 -0600

    latest

--- sample/build-image.sh
+++ sample/build-image.sh
@@ -2,6 +2,6 @@
 
 cd ../
 ./gradlew clean fatJar
-cp build/libs/docker-discovery-registrator-consul-1.0-RC1-fat.jar sample/
+cp build/libs/docker-discovery-registrator-consul-1.0-RC1-fat.jar sample/sample.jar
 cd sample/
 docker build --rm=true -t docker-discovery-registrator-consul-sample .
